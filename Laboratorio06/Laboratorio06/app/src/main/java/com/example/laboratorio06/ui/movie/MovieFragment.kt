317ec16eebb9ca9682e82f0bc136a7b98f4a38a3
samportan <samijtucker@gmail.com>
2023-04-25 12:06:47 -0600

    labo06 added

---
+++ Laboratorio06/Laboratorio06/app/src/main/java/com/example/laboratorio06/ui/movie/MovieFragment.kt
@@ -0,0 +1,71 @@
+package com.example.laboratorio06.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.laboratorio06.R
+import com.example.laboratorio06.databinding.FragmentMovieBinding
+
+class MovieFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentMovieBinding
+    private val viewModel: MovieViewModel by viewModels {
+        MovieViewModel.factory
+    }
+    private var onBackPressedCallback: OnBackPressedCallback? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie, container, false)
+
+        val movieName = arguments?.getString("movieName")
+        val movieCategory = arguments?.getString("movieCat")
+        val movieDescription = arguments?.getString("movieDesc")
+        val movieQ = arguments?.getString("movieQ")
+
+        binding.displayNameTextview.text = movieName
+        binding.displayCatTextview.text = movieCategory
+        binding.displayDescTextview.text = movieDescription
+        binding.displayQualiTextview.text = movieQ
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().navigateUp()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+            onBackPressedCallback as OnBackPressedCallback
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        onBackPressedCallback?.remove()
+        onBackPressedCallback = null
+    }
+
+
+}
